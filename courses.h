aa42968aa10a218b5b3285d18b54a4cf856597cc
rbautistajr10 <ramonbautistajr@hotmail.com>
2016-12-04 10:28:48 -0800

    Ramon Bautista

--- courses.h
+++ courses.h
@@ -21,12 +21,6 @@ public:
 	~CoursesList();
 	void AddToList();
 	void ViewList();
-	//void MovetoEnd();
-	//int TotalCourses();
 private:
 	Courses* head;
-	//int TotalCourses(Courses*);
-	/*void InsertBeginning(Employee*, Employee*); this is the only thing i added to the header file because
-	i figure it would make it easier to input the current node to the top of the previous node*/
-	// add any other private member functions you wish - not required
 };
