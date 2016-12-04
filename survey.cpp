ba4fd25f4907b80a75b82065557a5e61ee4f8b6b
shortaflip <kevin_enario@yahoo.com>
2016-12-04 10:19:43 -0800

    Add files via upload

---
+++ survey.cpp
@@ -0,0 +1,30 @@
+survey.cpp
+
+#include "survey.h"
+
+survey::survey()
+{
+	Cname = "Null";
+	major = "Null";
+	Trating = 0;
+	difficulty = 0;
+}
+
+void survey::getvalues(string name, string m, int r, int d)
+{
+	Cname = name;
+	major = m;
+	Trating = r;
+	difficulty = d;
+}
+
+void survey::display()
+{
+	cout << "\nCourse name: ";
+	cout << Cname;
+	cout << "\nStudents major: ";
+	cout << major;
+	cout << "\nProfessor rating, 0-5, '0' being the lowest: ";
+	cout << Trating;
+	cout << "\nClass rating, 0-5, '0' being the lowest: ";
+	cout << difficulty;
---
+++ survey.h
@@ -0,0 +1,18 @@
+survey.h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class survey		
+{
+private:
+	string Cname, major;
+	int Trating, difficulty;
+
+public:
+	survey();	
+	void getvalues(string, string, int, int);
+	void display();
+};
